22df48c69f7ae5ce8f09e6c754d19620e9aa79aa
Nagamani11 <gugulothunagamani96@gmail.com>
2026-07-24 12:42:43 +0530

    initial changes

--- vite.config.ts
+++ vite.config.ts
@@ -88,7 +88,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Sneha & Prahar Consultancy Services"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
